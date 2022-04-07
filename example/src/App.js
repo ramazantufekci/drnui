7e2ecb9ac13346893fdac001f9ef461641663b7b
ramazantufekci <ramazan.tufekci@outlook.com>
2022-04-07 12:09:50 +0300

    button library

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
-
-import { ExampleComponent } from 'drnui'
-import 'drnui/dist/index.css'
+import React from 'react';
+import {Button} from 'drnui';
+import 'drnui/dist/index.css';
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+	return (
+	<div className="test">
+		<Button text="Primary Button" type="primary"/>
+		<Button text="Default Button" type="default"/>
+		<Button text="Dashed Button" type="dashed"/>
+		<Button text="Text Button" type="text"/>
+		<Button text="Link Button" type="link"/>
+	</div>
+	);
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = ({ text,type}) => {
+  if(type==="primary"){
+     return <button className={styles.primary}>{text}</button>
+  }else if(type==="dashed"){
+     return <button className={styles.dashed}>{text}</button>
+  }else if(type==="text"){
+    return <button className={styles.text}>{text}</button>
+  }else if(type==="link"){
+    return <button className={styles.link}>{text}</button>
+  }
+  return <button className={styles.default}>{text}</button>
 }
